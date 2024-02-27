ab7baef314b4fe7972d0460135e7dd85bc867963
jjromero <jjromero@mecalux.com>
2024-02-27 23:17:28 +0100

    Generación de código de paletas, lectura de ubicaciones y contenedores

---
+++ Assets/Scripts/rowshell.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rowshell : MonoBehaviour
+{
+    public IEnumerable<shelf> shelfs;
+    public int minNumSSCC;
+    public int maxNumSSCC;
+
+    public void Awake()
+    {
+        IEnumerable<pallet> pallets = GetComponentsInChildren<pallet>();
+        // Creamos una lista para almacenar los valores asignados
+        HashSet<int> assignedValues = new HashSet<int>();
+
+        foreach (var item in pallets)
+        {
+            int randomValue = GetUniqueRandomValue(minNumSSCC, maxNumSSCC, assignedValues);
+            item.ssc = randomValue.ToString().PadLeft(4, '0');
+        }
+
+    }
+
+
+
+    int GetUniqueRandomValue(int min, int max, HashSet<int> assignedValues)
+    {
+        int randomValue = UnityEngine.Random.Range(min, max + 1);
+        while (assignedValues.Contains(randomValue))
+        {
+            randomValue = UnityEngine.Random.Range(min, max + 1);
+        }
+        assignedValues.Add(randomValue);
+        return randomValue;
+    }
+
+}
